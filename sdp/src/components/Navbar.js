1524d194ca801c1db5482cd2d4150fa77b6bbe76
shashank lalit patil <shashankpatil125@gmail.com>
2022-04-28 14:12:43 +0000

    tp

---
+++ sdp/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='h-14  bg-neutral-100'>
+        <p className='text-2xl mt-3'>Name</p>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ sdp/src/pages/Chats.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar.js'
+
+function Chats() {
+  return (
+    <div className="">
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Chats
